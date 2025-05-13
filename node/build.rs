90efcaf20faea91512e2007fcfb96dc7d13de09c
amirnairdrop2 <amirnairdrop2@gmail.com>
2025-05-13 21:10:24 +0330

    Update build.rs

--- node/build.rs
+++ node/build.rs
@@ -3,7 +3,7 @@
 // Copyright (C) Parity Technologies (UK) Ltd.
 // SPDX-License-Identifier: GPL-3.0-or-later WITH Classpath-exception-2.0
 
-// This program is free software: you can redistribute it and/or modify
+// This program is free software: you can redistribute it and/oR modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
